4da087053356614fc740d7f9c34d92f8ae4fdfcd
Jorge Luis <allku.expert@gmail.com>
2021-04-02 18:58:06 -0500

    Added hostname in UUID primary key in tables (taxlines, tickets, receipts, ticketlines, payments)

--- src/main/java/com/openbravo/pos/ticket/TicketInfo.java
+++ src/main/java/com/openbravo/pos/ticket/TicketInfo.java
@@ -1,21 +1,22 @@
-//    uniCenta oPOS  - Touch Friendly Point Of Sale
-//    Copyright (c) 2009-2018 uniCenta
-//    https://unicenta.com
+//    Allku Pos  - Touch Friendly Point Of Sale
+//    Copyright (c) 2009-2018 uniCenta & previous Openbravo POS works
+//    https://www.allku.expert
 //
-//    This file is part of uniCenta oPOS
+//    This file is part of Allku Pos
 //
-//    uniCenta oPOS is free software: you can redistribute it and/or modify
+//    Allku Pos is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//   uniCenta oPOS is distributed in the hope that it will be useful,
+//    Allku Pos is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License
-//    along with uniCenta oPOS.  If not, see <http://www.gnu.org/licenses/>.
+//    along with Allku Pos.  If not, see <http://www.gnu.org/licenses/>.
+
 package com.openbravo.pos.ticket;
 
 import com.openbravo.basic.BasicException;
@@ -91,7 +92,7 @@ public final class TicketInfo implements SerializableRead, Externalizable {
 
     /** Creates new TicketModel */
     public TicketInfo() {
-        m_sId = UUID.randomUUID().toString();
+        m_sId = UUID.randomUUID().toString() + "-" + Hostname;
         tickettype = RECEIPT_NORMAL;
         m_iTicketId = 0; // incrementamos
         m_dDate = new Date();
